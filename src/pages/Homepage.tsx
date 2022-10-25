6da17bfdb0a838bd95a368a3ff3d61dae54bb6da
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-10-25 19:43:16 +0200

    commit css

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,8 +1,90 @@
 export function Homepage(){
     return(
+        //hardcoded until fetched from the server
         <div className="home">
         <input placeholder="Search"></input>
-        <div className="newest-titles">
+        <h1>Newest Titles</h1>
+        <div className="titles">
+            <div className="list-book">
+                <img src="https://m.media-amazon.com/images/I/71Le9HD+HuL.jpg"></img>
+                <h3>Letters to Milena</h3>
+                <p className="author">Franz Kafka</p>
+                <h3>XX Price</h3>
+                <button>See details</button>
+            </div>
+            <div className="list-book">
+                <img src="https://m.media-amazon.com/images/I/71Le9HD+HuL.jpg"></img>
+                <h3>Letters to Milena</h3>
+                <p className="author">Franz Kafka</p>
+                <h3>XX Price</h3>
+                <button>See details</button>
+            </div>
+            <div className="list-book">
+                <img src="https://m.media-amazon.com/images/I/71Le9HD+HuL.jpg"></img>
+                <h3>Letters to Milena</h3>
+                <p className="author">Franz Kafka</p>
+                <h3>XX Price</h3>
+                <button>See details</button>
+            </div>
+            <div className="list-book">
+                <img src="https://m.media-amazon.com/images/I/71Le9HD+HuL.jpg"></img>
+                <h3>Letters to Milena</h3>
+                <p className="author">Franz Kafka</p>
+                <h3>XX Price</h3>
+                <button>See details</button>
+            </div>
+            <div className="list-book">
+                <img src="https://m.media-amazon.com/images/I/71Le9HD+HuL.jpg"></img>
+                <h3>Letters to Milena</h3>
+                <p className="author">Franz Kafka</p>
+                <h3>XX Price</h3>
+                <button>See details</button>
+            </div>
+            <div className="list-book">
+                <img src="https://m.media-amazon.com/images/I/71Le9HD+HuL.jpg"></img>
+                <h3>Letters to Milena</h3>
+                <p className="author">Franz Kafka</p>
+                <h3>XX Price</h3>
+                <button>See details</button>
+            </div>
+        </div>
+        <h1>Top Rated</h1>
+        <div className="titles">
+            <div className="list-book">
+                <img src="https://m.media-amazon.com/images/I/71Le9HD+HuL.jpg"></img>
+                <h3>Letters to Milena</h3>
+                <p className="author">Franz Kafka</p>
+                <h3>XX Price</h3>
+                <button>See details</button>
+            </div>
+            <div className="list-book">
+                <img src="https://m.media-amazon.com/images/I/71Le9HD+HuL.jpg"></img>
+                <h3>Letters to Milena</h3>
+                <p className="author">Franz Kafka</p>
+                <h3>XX Price</h3>
+                <button>See details</button>
+            </div>
+            <div className="list-book">
+                <img src="https://m.media-amazon.com/images/I/71Le9HD+HuL.jpg"></img>
+                <h3>Letters to Milena</h3>
+                <p className="author">Franz Kafka</p>
+                <h3>XX Price</h3>
+                <button>See details</button>
+            </div>
+            <div className="list-book">
+                <img src="https://m.media-amazon.com/images/I/71Le9HD+HuL.jpg"></img>
+                <h3>Letters to Milena</h3>
+                <p className="author">Franz Kafka</p>
+                <h3>XX Price</h3>
+                <button>See details</button>
+            </div>
+            <div className="list-book">
+                <img src="https://m.media-amazon.com/images/I/71Le9HD+HuL.jpg"></img>
+                <h3>Letters to Milena</h3>
+                <p className="author">Franz Kafka</p>
+                <h3>XX Price</h3>
+                <button>See details</button>
+            </div>
             <div className="list-book">
                 <img src="https://m.media-amazon.com/images/I/71Le9HD+HuL.jpg"></img>
                 <h3>Letters to Milena</h3>
