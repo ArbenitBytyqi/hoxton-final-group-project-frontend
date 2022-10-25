56db4916fd2962ad0da2c68dc23ab2de121cd1a9
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-10-25 22:22:11 +0200

    cart page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { BookDetails } from "./pages/BookDetails";
+import { Cart } from "./pages/Cart";
 import { Homepage } from "./pages/Homepage";
 import { LogIn } from "./pages/LogIn";
 import { PageNotFound } from "./pages/PageNotFound";
@@ -16,8 +17,9 @@ function App() {
         <Routes>
           <Route index element={<Navigate to="/home" />} />
           <Route path="/home" element={<Homepage />} />
-          <Route path="/bookdetails" element={<BookDetails />} />
+          <Route path="/bookdetails/:itemId" element={<BookDetails />} />
           <Route path="/login" element={<LogIn />} />
+          <Route path="/cart" element={<Cart />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </main>
---
+++ src/pages/Cart.tsx
@@ -0,0 +1,30 @@
+export function Cart() {
+  return (
+    <div className="cart">
+      <h2>Wish List</h2>
+      <ul>
+        <li>
+          <img src="https://m.media-amazon.com/images/I/71Le9HD+HuL.jpg" className="cart-book"></img>
+          <p>Title</p>
+          <p>Author</p>
+          <p>Price</p>
+          <span className="material-symbols-outlined">delete</span>
+        </li>
+        <li>
+          <img src="https://m.media-amazon.com/images/I/71Le9HD+HuL.jpg" className="cart-book"></img>
+          <p>Title</p>
+          <p>Author</p>
+          <p>Price</p>
+          <span className="material-symbols-outlined">delete</span>
+        </li>
+        <li>
+          <img src="https://m.media-amazon.com/images/I/71Le9HD+HuL.jpg" className="cart-book"></img>
+          <p>Title</p>
+          <p>Author</p>
+          <p>Price</p>
+          <span className="material-symbols-outlined">delete</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
