9a08c8b1025f18a0600f36a20e170780f1098c8f
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-10-25 20:34:51 +0200

    css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+
 export function Header() {
   return (
     <header>
-    <h3>NAME OF LIBRARY</h3>
-    <h3>My account</h3>
-  </header>
+      <Link to="/home">
+        <h3>NAME OF LIBRARY</h3>
+      </Link>
+      <div className="my-account">
+        <p>My account</p>
+        <span className="material-symbols-outlined">account_circle</span>
+      </div>
+    </header>
   );
 }
