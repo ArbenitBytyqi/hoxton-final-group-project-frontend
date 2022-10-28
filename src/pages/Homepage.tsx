2151315d214ee6a65226a0775810bd1890220bc6
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-10-28 13:23:36 +0200

    top rated section

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,22 +2,23 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export function Homepage({ allBooks, setSelectedBook }: any) {
-
-  const [searchBooks, setSearchBooks] = useState('')
-  const topRated: any = []
+  const [searchBooks, setSearchBooks] = useState("");
+  const topRated: any = [];
 
   for (let book of allBooks) {
-    let sum = 0
+    let sum = 0;
     for (const review of book.reviews) {
-      if (review.stars > 4.5) sum += review.stars
-      let average = sum / book.reviews.length
-      if (average > 3) topRated.push(book)
+      if (review.stars > 4.5) sum += review.stars;
+      let average = sum / book.reviews.length;
+      if (average > 3) topRated.push(book);
     }
   }
 
-  const filteredBooks = allBooks.filter(filteredAllBooks => filteredAllBooks.title.toLowerCase().includes(searchBooks.toLowerCase()))
-  console.log(`topRated`, topRated)
-  console.log("searchBooks", filteredBooks)
+  const filteredBooks = allBooks.filter((filteredAllBooks) =>
+    filteredAllBooks.title.toLowerCase().includes(searchBooks.toLowerCase())
+  );
+  console.log(`topRated`, topRated);
+  console.log("searchBooks", filteredBooks);
   return (
     //hardcoded until fetched from the server
     <div className="home">
@@ -26,37 +27,56 @@ export function Homepage({ allBooks, setSelectedBook }: any) {
           type="text"
           name="search"
           placeholder="Search here..."
-          onChange={event => {
-            setSearchBooks(event.target.value)
-            console.log("event", setSearchBooks)
+          onChange={(event) => {
+            setSearchBooks(event.target.value);
+            console.log("event", setSearchBooks);
           }}
         />
 
         <button>🔎</button>
       </div>
-      <h1>Top Rated</h1>
+      {searchBooks === "" ? <h1>Top Rated</h1> : null}
+
       <div className="titles">
-        {topRated.map(book => <div className="list-book">
-          <img src={book.image}></img>
-          <h3>{book.title}</h3>
-          <p className="author">Franz Kafka</p>
-          <h3>{book.price}</h3>
-          <Link to={`/bookdetails`}>
-            <button onClick={() => { setSelectedBook(book) }}>See details</button>
-          </Link>
-        </div>)}
+        {searchBooks === ""
+          ? topRated.map((book) => (
+              <div className="list-book">
+                <img src={book.image}></img>
+                <h3>{book.title}</h3>
+                <p className="author">Franz Kafka</p>
+                <h3>{book.price}</h3>
+                <Link to={`/bookdetails`}>
+                  <button
+                    onClick={() => {
+                      setSelectedBook(book);
+                    }}
+                  >
+                    See details
+                  </button>
+                </Link>
+              </div>
+            ))
+          : null}
       </div>
       <h1>Books</h1>
       <div className="titles">
-        {filteredBooks.map(book => <div className="list-book">
-          <img src={book.image}></img>
-          <h3>{book.title}</h3>
-          <p className="author">Franz Kafka</p>
-          <h3>{book.price}</h3>
-          <Link to={`/bookdetails`}>
-            <button onClick={() => { setSelectedBook(book) }}>See details</button>
-          </Link>
-        </div>)}
+        {filteredBooks.map((book) => (
+          <div className="list-book">
+            <img src={book.image}></img>
+            <h3>{book.title}</h3>
+            <p className="author">Franz Kafka</p>
+            <h3>{book.price}</h3>
+            <Link to={`/bookdetails`}>
+              <button
+                onClick={() => {
+                  setSelectedBook(book);
+                }}
+              >
+                See details
+              </button>
+            </Link>
+          </div>
+        ))}
       </div>
     </div>
   );
